fb6a37d0488f8b2d2d7ff97f004b52f44f7d4f86
marianedossantosassis28-source <marianedossantosassis28@gmail.com>
2026-06-26 14:32:42 -0300

    aula dia 26

---
+++ exercicioJs/ex01.js
@@ -0,0 +1,11 @@
+// Faça um programa que imprima:
+
+// Meu nome é (seu nome)
+// Tenho (sua idade)
+// Estou aprendendo JavaScript
+
+let idade = 17
+
+console.log("Meu nome é: Mariane");
+console.log("Tenho %d anos", idade);
+console.log("Estou aprendendo JavaScript"); 
