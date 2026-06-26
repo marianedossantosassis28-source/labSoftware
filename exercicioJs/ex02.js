271470f78b4839e9b9093aa45371d8cc5400254c
marianedossantosassis28-source <marianedossantosassis28@gmail.com>
2026-06-26 16:30:35 -0300

    foi feito a atividade

--- exercicioJs/ex02.js
+++ exercicioJs/ex02.js
@@ -8,3 +8,15 @@
 // cidade
 
 // Depois imprima todas elas e em seguida imprima seus tipos.
+
+const nome = "Mariane";
+const sobrenome = "Mari";
+let idade = "17";
+const cidade = "Alfredo V."
+
+console.log(nome, sobrenome, idade, cidade);
+
+console.log(typeof nome);
+console.log(typeof sobrenome);
+console.log(typeof idade);
+console.log(typeof cidade);
--- exercicioJs/ex03.js
+++ exercicioJs/ex03.js
@@ -9,3 +9,10 @@
 // Mostre no console usando concatenando as variáveis:
 // Meu (produto) custa (preco) e temos (quantidade) no estoque
 
+const produto = "biscoito";
+const preco = "10";
+const quantidade = "2";
+
+console.log("Meu %s é:", produto);
+console.log("custa %d:", preco);
+console.log("temos %d no estoque:",quantidade);
